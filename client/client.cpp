a20fea6a2da9536dae3d20ff5bcbdac6b2ebd38b
DShcherbak <denys.shcherbak.official@gmail.com>
2020-05-16 00:42:31 +0300

    Implemented client
    It takes 2 ports as command-lien arguments and creates a client that listens on the second and sends commands on the first one

---
+++ client/client.cpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <fstream>
+#include "../server/Server.hpp"
+
+int getPortName(char* argument){
+    int id = 0;
+    int result = 0;
+    while(argument[id]!='\0'){
+        if(!isdigit(argument[id]))
+            return -1;
+        result *= 10;
+        result += (int) (argument[id] - '0');
+        id++;
+        if(id > 6)
+            return -1;
+    }
+    if(result < 1000 || result > 9999)
+        return -1;
+    return result;
+}
+
+int main(int argc, char** argv) {
+    Server server;
+    Server server2;
+
+    std::cout << argv[0] << "|" << argv[1] << "|" << argv[2] << std::endl;
+    int serverPortName = getPortName(argv[1]);
+    int myPortName = getPortName(argv[2]);
+    std::string filename = "logs/errorLog_" + std::string(argv[1]) + "_" + std::string(argv[2]) + ".txt";
+    const char* charFilename = filename.c_str();
+    std::ofstream errorLog(charFilename);
+    if(serverPortName == -1){
+        errorLog << "CLIENT ERROR: Incorrect server port name :" << argv[1] << std::endl;
+        return 0;
+    }
+    if(myPortName == -1){
+        errorLog << "CLIENT ERROR: Incorrect client port name :" << argv[2] << std::endl;
+        return 0;
+    }
+    std::cout << "Bot listening on: " << myPortName << std::endl;
+    // std::ofstream ofs("log.txt");
+    std::string message;
+    while(message != "-1") {
+        message = server.receive(myPortName);
+        usleep(100);
+        std::cout << message;
+        getline(std::cin, message);
+        server.send(serverPortName, message);
+    }
+    return 0;
+}
