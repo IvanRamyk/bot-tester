61de52429ef2520dac1b00037d331bf0424d125c
IvanRamyk <ivan.ramyk.official@gmail.com>
2020-06-06 16:53:18 +0300

    Merge branch 'master' of https://github.com/DShcherbak/bot-tester into DShcherbak-master
    # Conflicts:
    #	CMakeLists.txt
    #	Interactor/Interactor.h
    #	game/Quoridor.cpp
    #	main.cpp
    #	rules.md

--- script1.sh
+++ script1.sh
@@ -1,1 +1,1 @@
-cat pipeline | ./client/cmake-build-debug/testClient 5036 5037 | ./players/team_7/cyber-contest/team_7 > pipeline
+cat pipeline | ./client/cmake-build-debug/testClient 5036 5037 | ./players/team_4/team_4 > pipeline
--- script2.sh
+++ script2.sh
@@ -1,1 +1,1 @@
-cat pipeline2 | ./client/cmake-build-debug/testClient 5036 5038 | ./players/team_6/team_6 > pipeline2
+cat pipeline2 | ./client/cmake-build-debug/testClient 5036 5038 | ./players/team_1/team_1 > pipeline2
